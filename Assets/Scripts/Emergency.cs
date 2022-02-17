5617646d5f4aad5c7d767cea11b9b8a4c746780c
Teun de Vries <493859@student.saxion.nl>
2022-02-17 13:31:54 +0100

    Stuffs (I forgor what I did)

--- Assets/Scripts/Emergency.cs
+++ Assets/Scripts/Emergency.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Emergency : MonoBehaviour
 {
     private Light light;
+    private SpriteRenderer _render;
     private float speed = 2;
     private Color defC;
     private bool _emergency = true;
@@ -13,23 +14,41 @@ public class Emergency : MonoBehaviour
     void Start()
     {
         light = GetComponent<Light>();
-        defC = light.color;
+        if (light == null)
+        {
+            _render = GetComponent<SpriteRenderer>();
+            defC = _render.color;
+        }
+        else
+        {
+            defC = light.color;
+        }
+        
     }
 
     public void StopEmergency()
     {
         _emergency = false;
-        light.color = defC;
+        if (light != null) light.color = defC;
+        else _render.color = defC;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_emergency)
+        if (_emergency && light != null)
         {
             Color c = Color.red;
             c.r *= Mathf.Abs(Mathf.Sin(Time.time * 2));
             light.color = c;
+        } else if (_emergency)
+        {
+            Color c = Color.red;
+            c.r *= Mathf.Abs(Mathf.Sin(Time.time * 2));
+            if (c.r < 0.5f) c.r = 0.5f;
+            c.g = 0.5f;
+            c.b = 0.5f;
+            _render.color = c;
         }
         
     }
--- Assets/Scripts/moveSolarSystem.cs
+++ Assets/Scripts/moveSolarSystem.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class moveSolarSystem : MonoBehaviour
 {
     public float speed;
+    [SerializeField] private bool useX;
+    [SerializeField] private bool useY;
+    [SerializeField] private bool useZ;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,13 @@ public class moveSolarSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (useX)
         transform.position = new Vector3((speed * Time.deltaTime) + transform.position.x, transform.position.y,transform.position.z);
+
+        if (useY)
+            transform.position = new Vector3(transform.position.x, (speed * Time.deltaTime) + transform.position.y, transform.position.z);
+
+        if (useZ)
+            transform.position = new Vector3(transform.position.x, transform.position.y, (speed * Time.deltaTime) + transform.position.z);
     }
 }
